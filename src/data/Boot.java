3955dad1df83a54fadd452d598032007172711fb
Ellirabeth <j.u.n.i.a@tut.by>
2016-02-19 00:11:19 +0300

    console window is open

--- src/data/Boot.java
+++ src/data/Boot.java
@@ -4,6 +4,8 @@ import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
+import static org.lwjgl.opengl.GL11.*;
+
 public class Boot {
 	public Boot() {
 		Display.setTitle("My_Title_game");
@@ -14,7 +16,20 @@ public class Boot {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, 600, 400, 0, 1, -1);
+		glMatrixMode(GL_MODELVIEW);
+
+		while(!Display.isCloseRequested()){
+
+
+			Display.update();
+			Display.sync(60);
+		}
+
+		Display.destroy();
 	}
 
 	public static void main (String[] arqs){
