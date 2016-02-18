f9e099dfc58a384880b9b328251c59b5e683762a
Ellirabeth <j.u.n.i.a@tut.by>
2016-02-19 01:07:18 +0300

    data.tile class beginning

--- src/data/Boot.java
+++ src/data/Boot.java
@@ -11,24 +11,12 @@ import static helpers.Artist.*;
 public class Boot {
 	public Boot() {
 		BeginSession();
-		
-		float width = 50;
-		float height = 50;
-		float x = 100;
-		float y = 100;
 
 		while(!Display.isCloseRequested()){
-			glBegin(GL_LINES);
-			glVertex2f(10, 10);
-			glVertex2f(100, 100);
-			glEnd();
+
 			
-			glBegin(GL_QUADS);
-			glVertex2f(x, y); //Top left corner
-			glVertex2f(x + width, y); //Top right corner
-			glVertex2f(x + width, y + height); //Bottom right corner
-			glVertex2f(x, y + height); //Bottom left corner
-			glEnd();
+			DrawSquad(50, 50, 100, 100);
+			DrawSquad(150, 150, 100, 100);
 
 			Display.update();
 			Display.sync(60);
