67f5e24d1c17950d16d2c3bf2b67b524d8e7173b
Ellirabeth <j.u.n.i.a@tut.by>
2016-02-19 00:53:29 +0300

    helper.artist class

--- src/data/Boot.java
+++ src/data/Boot.java
@@ -6,21 +6,11 @@ import org.lwjgl.opengl.DisplayMode;
 
 import static org.lwjgl.opengl.GL11.*;
 
+import static helpers.Artist.*;
+
 public class Boot {
 	public Boot() {
-		Display.setTitle("My_Title_game");
-		try {
-			Display.setDisplayMode(new DisplayMode(600, 400));
-			Display.create();
-		} catch (LWJGLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		glOrtho(0, 600, 400, 0, 1, -1);
-		glMatrixMode(GL_MODELVIEW);
+		BeginSession();
 		
 		float width = 50;
 		float height = 50;
