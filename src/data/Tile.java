f85e151cb4a7bf49ebadf608db7d19703a15161e
Ellirabeth <j.u.n.i.a@tut.by>
2016-02-19 01:48:49 +0300

    data.tile beginning

--- src/data/Tile.java
+++ src/data/Tile.java
@@ -1,5 +1,16 @@
 package data;
 
+import org.newdawn.slick.opengl.Texture;
 public class Tile {
 
+		private float x,y,width,height;
+		private Texture texture;
+		
+		public Tile(float x, float y, float width, float height, Texture texture ){
+			this.x = x;
+			this.y = y;
+			this.height = height;
+			this.width = width;
+			
+		}
 }
